dc4529c21471642645064fe0195dc7d025fe2ae2
KARRI VIJAY <vijaykarri220910125@gmail.com>
2025-09-05 22:01:04 +0530

    Update App.jsx

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -8,7 +8,7 @@ import Oders from './pages/Oders/Oders'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 const App = () => {
-    const url="http://localhost:4000"
+    const url="https://food-app-backend-bzfm.onrender.com"
   return (
     <div> 
       <ToastContainer/>
